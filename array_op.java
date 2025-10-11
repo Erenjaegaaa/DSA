8143d3bedd52c1c6a7b04dae3afb38053a03c154
Erenjaegaaa <saahilmanojmp@ug.cusat.ac.in>
2025-10-11 23:55:08 +0530

    ..

--- array_op.java
+++ array_op.java
@@ -1,48 +1,48 @@
-
 import java.util.Scanner;
+
 public class array_op {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        int [] arr=new int[100];
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
         System.out.print("enter the no of elements:  ");
-        int num=sc.nextInt();
+        int num = sc.nextInt();
 
-        if(num>100||num<0){
+        if (num > 100 || num < 0) {
             System.out.print("invalid");
             return;
         }
-        System.out.print("enter the elem:");
-        for(int i=0;i<num;i++){
-            arr[i]=sc.nextInt();
-           
-        }
-        for(int i=0;i<num;i++){
-            System.out.print(arr[i]);
 
+        System.out.print("enter the elem: ");
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.print("enter pos: ");
-        int pos=sc.nextInt();
-        if(pos>num||pos<0){
+
+        for (int i = 0; i < num; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.print("\nenter pos: ");
+        int pos = sc.nextInt();
+
+        if (pos > num + 1 || pos <= 0) {   // ✅ fixed bounds
             System.out.print("invalid");
             return;
         }
+
         System.out.print("enter element: ");
-        int elem=sc.nextInt();
-        for(int i=num-1;i>=pos-1;i--){
-            
-            arr[i+1]=arr[i];
+        int elem = sc.nextInt();
 
+        for (int i = num - 1; i >= pos - 1; i--) {  // shifting
+            arr[i + 1] = arr[i];
         }
-        arr[pos-1]=elem;
+
+        arr[pos - 1] = elem;   // ✅ fixed off-by-one index
         num++;
-        for(int i=0;i<num;i++){
-            System.out.print("new array"+arr[i]);
 
+        System.out.print("new array: ");
+        for (int i = 0; i < num; i++) {
+            System.out.print(arr[i] + " ");
         }
-        
-
-
-
     }
 }
