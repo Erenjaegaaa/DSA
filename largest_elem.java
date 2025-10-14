aca5f432e25a74fec3ecc200a97a00734b97624e
Erenjaegaaa <saahilmanojmp@ug.cusat.ac.in>
2025-10-15 00:15:18 +0530

    .

--- largest_elem.java
+++ largest_elem.java
@@ -1,3 +1,23 @@
 public class largest_elem {
+    public static void main(String[]args){
+        
+
     
+
+
+
+
+    public int largestElement(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
+
+}
+
+}
+
 }
